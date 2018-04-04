a65f3f1fb9a466aa75b2863f5a9d52242e7be753
vince4128 <vrlt.vincent@gmail.com>
2018-04-04 10:17:07 +0200

    forEach Continued

--- app/foreachHelper/foreachHelper.js
+++ app/foreachHelper/foreachHelper.js
@@ -1,5 +1,24 @@
-export function foreachHelper(array){
-    array.forEach(function(item){
-        console.log(item);
+export function foreachHelper(){
+
+    //create an array
+    const colors = ['red','green','blue'];
+    //loop over the array
+    colors.forEach(function(color){
+        console.log(color);
     });
+
+    //create an array
+    const numbers = [1,2,3,4,5];
+    //create a variable to hold the sum
+    var sum = 0;
+
+    function adder(number){
+        sum += number;
+    }
+
+    //Loop over the array, incrementing the sum variable
+    numbers.forEach(adder);
+    //print the sum variable
+    console.log(sum);
+
 }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
 //forEach
 
 import { foreachHelper } from "./foreachHelper/foreachHelper";
-
-var colors = ['red', 'blue', 'green'];
-foreachHelper(colors);
+foreachHelper();
