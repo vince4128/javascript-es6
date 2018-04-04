267585cd900d205ae84bd2ec40d3d3b150d9244d
vince4128 <vrlt.vincent@gmail.com>
2018-04-04 13:06:59 +0200

    map helper continued

--- app/mapHelper/mapHelper.js
+++ app/mapHelper/mapHelper.js
@@ -1,10 +1,22 @@
 export function mapHelper(){
 
     const numbers = [1,2,3];
-    var doubleNumbers = numbers.map(function(number){
+    //new var, avoid mutating numbers array
+    var doubleNumbers = numbers.map((number) => {
         return number * 2;
     });
 
     console.log(doubleNumbers);
+
+    const cars = [
+        { model: '2cv', price: 'CHEAP'},
+        { model : 'sm', price: 'expensive'}
+    ];
+
+    const prices = cars.map((car)=>{
+        return car.price;
+    });
+
+    console.log(prices);
 
 }
