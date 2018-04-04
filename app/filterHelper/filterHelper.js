ba2ad1585e15c3007d7b07aa00b182129dad9176
vince4128 <vrlt.vincent@gmail.com>
2018-04-04 13:56:14 +0200

    more filters

--- app/filterHelper/filterHelper.js
+++ app/filterHelper/filterHelper.js
@@ -2,10 +2,10 @@ export function filterHelper(){
 
     //selecting needed data with filter
     const products = [
-        {name:'concombre', type:'légume'},
-        {name:'banane', type:'fruit'},
-        {name:'céleri', type:'légume'},
-        {name:'orange', type:'fruit'}
+        {name:'concombre', type:'légume', quantity:0, price:1},
+        {name:'banane', type:'fruit', quantity:10, price:15},
+        {name:'céleri', type:'légume', quantity:30, price:9},
+        {name:'orange', type:'fruit', quantity:3, price:5}
     ];
     
     //filter to get only the object with a type of fruit
@@ -14,5 +14,12 @@ export function filterHelper(){
     });
 
     console.log(fruits);
+
+    // Type is 'vegetable', quantity is greater than 0, price is less than 10
+    var cheapVegeAvailable = products.filter((product)=>{
+        return product.type === 'fruit' && product.quantity>0 && product.price<10;
+    });
+
+    console.log(cheapVegeAvailable);
 
 };
