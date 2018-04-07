23b991e21c4e342a0f6de293b8ced41adfe664fd
vince4128 <vrlt.vincent@gmail.com>
2018-04-07 21:17:22 +0200

    use default function arguments

--- app/defaultFuncArgs/defaultFuncArgs.js
+++ app/defaultFuncArgs/defaultFuncArgs.js
@@ -6,11 +6,30 @@ export function defaultFuncArgs(){
     }
 
     makeAjaxRequest('google.com');
-    //makeAjaxRequest('google.com', null);
+    //makeAjaxRequest('google.com', undefined);
     makeAjaxRequest('google.com', null);
     makeAjaxRequest('google.com', 'POST');
 
     //use case
     
+    function User(id){
+        this.id = id;
+    }
+
+    function generateId(){
+        return Math.random()*999999;
+    }
+
+    function createAdminUser(user = new User(generateId())){
+        user.admin = true;
+        
+        return user;
+    }
+
+    createAdminUser();
+
+    console.log(createAdminUser());
+
+    const user = new User(generateId());
 
 }
